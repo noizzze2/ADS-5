dbed1cd9dc8528bf817edae23e0c2baee687d6ef
noizzze2 <noizzze20@gmail.com>
2026-04-19 13:16:54 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[size];
+    int top_index;
+
+ public:
+    TStack() : top_index(-1) {}
+
+    bool isEmpty() const {
+        return top_index == -1;
+    }
+
+    bool isFull() const {
+        return top_index == size - 1;
+    }
+
+    void push(const T& value) {
+        if (!isFull()) {
+            data[++top_index] = value;
+        }
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            return data[top_index--];
+        }
+        return T();
+    }
+
+    T top() const {
+        if (!isEmpty()) {
+            return data[top_index];
+        }
+        return T();
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
